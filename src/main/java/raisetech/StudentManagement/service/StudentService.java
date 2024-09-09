7724c859ec64ca403b6eb1fcc5cf6c8375c563fe
moustim320 <user@yuzanoMacBook-Air.local>
2024-09-09 18:33:55 +0900

    9/9までの変更を送信

--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -2,10 +2,13 @@ package raisetech.StudentManagement.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import raisetech.StudentManagement.data.StudentsCourses;
 import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.domein.StudentDetail;
 import raisetech.StudentManagement.repository.StudentRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,5 +27,17 @@ public class StudentService {
     public List<StudentsCourses> searchStudentsCourseList(){
         return repository.explore();
     }
+
+    @Transactional
+    public void registerStudent(StudentDetail studentDetail) {
+        repository.registerStudent(studentDetail.getStudent());
+        //TODO:コース情報登録も行う
+        for(StudentsCourses studentsCourses:studentDetail.getStudentsCourses()){
+            studentsCourses.setStudentId(studentDetail.getStudent().getId());
+            studentsCourses.setCourseStartAt(LocalDateTime.now());
+            studentsCourses.setCourseEndAt(LocalDateTime.now().plusYears(1));
+            repository.registerStudentsCourses(studentsCourses);
+        }
+        }
 
 }
